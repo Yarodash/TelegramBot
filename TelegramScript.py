d5abbdc23b1620fb76c8ed8716cc6e6f17641fdf
Yarodash <yaroslav.lukyanets@gmail.com>
2017-08-29 21:42:56 +0300

    Fixed some bug
    Fixed some bug

--- TelegramScript.py
+++ TelegramScript.py
@@ -4,14 +4,14 @@ import TelegramToken
 bot = telebot.TeleBot(TelegramToken.Token)
 
 def SendMessage(message):
-    print('Message to',userid.from_user.first_name,userid.from_user.first_name,message)
+    print('Message to',userid.from_user.first_name,userid.from_user.last_name,message)
     bot.send_message(userid.chat.id,message)
 
 @bot.message_handler(content_types=['text'])
 def handler_text(user):
         print('--------')
         message = user.text
-        print('Message from', user.from_user.first_name, user.from_user.first_name, message)
+        print('Message from', user.from_user.first_name, user.from_user.last_name, message)
 
         global userid
         userid = user
