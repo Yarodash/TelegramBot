c0e75f0503731bb84932d2ddff91326d17587ca3
Yarodash <yaroslav.lukyanets@gmail.com>
2017-08-29 18:49:50 +0300

    Skeleton of TelegramBot
    Created a Skeleton of TelegramBot

---
+++ TelegramScript.py
@@ -0,0 +1,25 @@
+import telebot
+import TelegramToken
+
+bot = telebot.TeleBot(TelegramToken.Token)
+
+def SendMessage(message):
+    print('Message to',userid.from_user.first_name,userid.from_user.first_name,message)
+    bot.send_message(userid.chat.id,message)
+
+@bot.message_handler(content_types=['text'])
+def handler_text(user):
+        print('--------')
+        message = user.text
+        print('Message from', user.from_user.first_name, user.from_user.first_name, message)
+
+        global userid
+        userid = user
+
+        if message == '/start':
+            SendMessage('/help to show available commands')
+
+        if message == '/help':
+            SendMessage('/help to show available commands')
+
+bot.polling(none_stop=True, interval=0)
